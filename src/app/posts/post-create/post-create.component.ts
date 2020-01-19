9d2c01b8cc5abbb9726386615ab85180d19eb988
WojciechGalecki <wojciech.galecki@wp.pl>
2020-01-19 10:14:19 +0100

    change post creation to template-driven-form

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,5 +1,6 @@
-import { Post } from './../post.model';
-import { Component, EventEmitter, Output} from '@angular/core';
+import {Post} from './../post.model';
+import {Component, EventEmitter, Output} from '@angular/core';
+import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-post-create',
@@ -12,10 +13,13 @@ export class PostCreateComponent {
 
   @Output() newPost = new EventEmitter<Post>();
 
-  onAddPost() {
+  onAddPost(form: NgForm) {
+    if (form.invalid) {
+      return;
+    }
     const post: Post = {
-      title: this.postTitle,
-      content: this.postContent
+      title: form.value.title,
+      content: form.value.content
     };
     this.newPost.emit(post);
   }
