f326e1c8c10e8969116a1a1b89ebd79a654fc289
WojciechGalecki <wojciech.galecki@wp.pl>
2020-01-27 14:23:03 +0100

    moved extracting file to the new middelware

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -1,56 +1,19 @@
 const express = require('express');
-const multer = require('multer');
 
 const checkAuth = require('../middleware/check-auth');
+const extractFile = require('../middleware/file');
 const PostsController = require('../controllers/posts');
 
 const router = express.Router();
 
-const MIME_TYPE_MAP = {
-  'image/png': 'png',
-  'image/jpeg': 'jpg',
-  'image/jpg': 'jpg'
-};
-
-const fileStorage = multer.diskStorage({
-  destination: (req, file, callback) => {
-    const isValid = MIME_TYPE_MAP[file.mimetype];
-    let error = new Error('Invalid mime type!');
-
-    if (isValid) {
-      error = null;
-    }
-    callback(error, 'backend/images') // relative path for server.js file
-  },
-  filename: (req, file, callback) => {
-    const name = file.originalname.toLowerCase().split(' ').join('-');
-    const ext = MIME_TYPE_MAP[file.mimetype];
-    callback(null, name + '-' + Date.now() + '.' + ext);
-  }
-});
-
-router.post(
-  '',
-  checkAuth,
-  multer({storage: fileStorage}).single('image'),
-  PostsController.addPost
-);
+router.post('', checkAuth, extractFile, PostsController.addPost);
 
 router.get('', PostsController.getAllPosts);
 
 router.get('/:id', PostsController.getPostById);
 
-router.put(
-  '/:id',
-  checkAuth,
-  multer({storage: fileStorage}).single('image'),
-  PostsController.updatePostById
-);
+router.put('/:id', checkAuth, extractFile, PostsController.updatePostById);
 
-router.delete(
-  '/:id',
-  checkAuth,
-  PostsController.deletePostById
-);
+router.delete('/:id', checkAuth, PostsController.deletePostById);
 
 module.exports = router;
